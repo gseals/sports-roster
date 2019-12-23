b3b3c548934ccc07156632cf669b7254d2262ebf
Gabriel Seals <sealsgj@gmail.com>
2019-12-22 23:25:21 -0600

    auth and nav complete

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/app';
 
 import firebaseConnection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
+import MyNavBar from '../components/MyNavBar/MyNavBar';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -17,53 +18,36 @@ class App extends React.Component {
   componentDidMount() {
     this.removeListener = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
-        this.setState ({ authed: true });
-        } else {
-          this.setState({ authed: false });
-        }
-      });
-    }
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
 
-    componentWillUnmount() {
-      this.removeListener();
-    }
+  componentWillUnmount() {
+    this.removeListener();
+  }
 
     renderView = () => {
       const { authed } = this.state;
       if (!authed) {
-        return(<Auth />);
+        return (<Auth />);
       }
     }
 
-    return (
-      <div className="App">
-      <MyNavBar authed={authed} />
-      {
-        this.renderView()
-      }
-    )
+    render() {
+      const { authed } = this.state;
 
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className='btn btn-danger'>This that test button</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      return (
+        <div className="App">
+          <MyNavBar authed={authed} />
+          {
+            this.renderView()
+          }
+        </div>
+      );
+    }
 }
 
 export default App;
