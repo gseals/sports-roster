bbe9e44a5a0ea8f1121ce9f060dd2f11c682a897
Gabriel Seals <sealsgj@gmail.com>
2019-12-25 14:22:49 -0600

    player form is hidden until button is pressed

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -14,6 +14,7 @@ import './Team.scss';
 class Team extends React.Component {
   state = {
     players: [],
+    showPlayerForm: false,
   }
 
   componentDidMount() {
@@ -40,15 +41,20 @@ class Team extends React.Component {
     playerData.createNewPlayer(newPlayer)
       .then(() => {
         this.getPlayers();
+        this.setState({ showPlayerForm: false });
       })
       .catch((errorFromSavePlayer) => console.error({ errorFromSavePlayer }));
+  }
+
+  setShowPlayerForm = () => {
+    this.setState({ showPlayerForm: true });
   }
 
   render() {
     return (
       <div className="d-flex flex-wrap justify-content-between">
-        {/* <button>Add a new player</button> */}
-        <PlayerForm addPlayer={this.addPlayer} />
+        <button onClick={this.setShowPlayerForm}>Add a new player</button>
+      {this.state.showPlayerForm && <PlayerForm addPlayer={this.addPlayer} /> }
         {this.state.players.map((player) => (<Player key={player.id} player={player} deleteSinglePlayer={this.deleteSinglePlayer}/>))}
       </div>
     );
